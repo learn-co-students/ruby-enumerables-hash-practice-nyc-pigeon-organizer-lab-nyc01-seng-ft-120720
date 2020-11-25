b422f3b4d9cdc11fc01056acb8f9845184322247
Kody Samaroo <kody.samaroo@gmail.com>
2020-11-25 06:27:50 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  data.each_with_object({}) do |(key, val), output|
+    val.each do |key2, val2|
+      val2.each do |name|
+        if !output[name]
+            output[name] = {}
+        end
+        if !output[name][key]
+           !output[name][key] = []
+        end
+      output[name][key].push(key2.to_s)
+      end 
+    end
+  end
 end
