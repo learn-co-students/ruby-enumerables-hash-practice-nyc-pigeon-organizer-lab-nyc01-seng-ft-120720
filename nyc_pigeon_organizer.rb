44b31125d6b8df632ffc98ff954bd8506d3e3d19
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-29 21:17:22 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,56 +1,19 @@
 require 'pry'
-pigeon_list = {
-  "Theo" => {
-    :color => ["purple", "grey"],
-    :gender => ["male"],
-    :lives => ["Subway"]
-  },
-  "Peter Jr." => {
-    :color => ["purple", "grey"],
-    :gender => ["male"],
-    :lives => ["Library"]
-  },
-  "Lucky" => {
-    :color => ["purple"],
-    :gender => ["male"],
-    :lives => ["Central Park"]
-  },
-  "Ms. K" => {
-    :color => ["grey", "white"],
-    :gender => ["female"],
-    :lives => ["Central Park"]
-  },
-  "Queenie" => {
-    :color => ["white", "brown"],
-    :gender => ["female"],
-    :lives => ["Subway"]
-  },
-  "Andrew" => {
-    :color => ["white"],
-    :gender => ["male"],
-    :lives => ["City Hall"]
-  },
-  "Alex" => {
-    :color => ["white", "brown"],
-    :gender => ["male"],
-    :lives => ["Central Park"]
-  }
-}
 
 def nyc_pigeon_organizer(data)
-  pigeon_list = {}
-  data.each do |color_gender_lives, value|
-    value.each do |stats, all_names|
-      all_names.each do |name|
-        if pigeon_list[name] == nil
-          pigeon_list[name] = {}
+ 
+  final_results = data.each_with_object({}) do |(key, value), final_array|
+    value.each do |inner_key, names| 
+      names.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
         end
-        if pigeon_list[name][color_gender_lives] == nil
-          pigeon_list[name][color_gender_lives] = []
+        if !final_array[name][key]
+        final_array[name][key] = []
         end
-        pigeon_list[name][color_gender_lives].push(stats.to_s)
-      end
-    end 
-  end
-  pigeon_list
-end
+        final_array[name][key].push(inner_key.to_s)
+      end 
+    end
+  end 
+end 
+
